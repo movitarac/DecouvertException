b610ee69b7137775fed0ff6174e606dfa7c8fcf4
movitarac <andriani.monicavita@gmail.com>
2018-03-19 16:10:09 +0100

    Add 1 exception

--- src/com/andriani/Program.java
+++ src/com/andriani/Program.java
@@ -1,10 +1,16 @@
 package com.andriani;
 
+import java.io.File;
+
 public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		try {
+			File.createTempFile("", null);
+		} catch (Exception erreur) {
+			erreur.printStackTrace();
+		}
 	}
 
 }
